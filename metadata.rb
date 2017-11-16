80321904a9a6c857f2349a0321853a898ffac652
Brian Hechinger <wonko@4amlunch.net>
2017-11-15 22:20:01 -0500

    Need to tweak the ldap config file a bit

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name              'openvpn'
-version           '3.0.21'
+version           '3.0.22'
 maintainer        'Xhost Australia'
 maintainer_email  'cookbooks@xhost.com.au'
 license           'Apache 2.0'
